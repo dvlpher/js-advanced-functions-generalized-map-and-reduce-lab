3458a3d44b3d5e1039da80c7ec65701541feb7da
CelesteSMThomas <celestesmthomas@gmail.com>
2021-03-22 12:49:32 -0700

    commit tree 5ea1a9471653fd1c581f651e77556a084f7584a5

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Add your functions here
+function map(sourceArray, callback) {
+  const newSourceArray = [];
+  for (let i = 0; i < sourceArray.length; i++) {
+    newSourceArray.push(callback(sourceArray[i]));
+  }
+  return newSourceArray;
+}
+
+function reduce(sourceArray, callback, starting) {
+  let newSourceArray = !!starting ? starting : sourceArray[0];
+  let i = !!starting ? 0 : 1;
+
+  for (; i < sourceArray.length; i++) {
+    newSourceArray = callback(sourceArray[i], newSourceArray);
+  }
+
+  return newSourceArray;
+}
